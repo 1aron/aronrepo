dd46e543a456ef1fd832d7bd20739f25a3d8d77b
1Aron <aron886tw@gmail.com>
2022-11-20 23:54:21 +0800

    Chore: Build

--- packages/semantic-release-config/src/index.ts
+++ packages/semantic-release-config/src/index.ts
@@ -1,2 +1,32 @@
-export * as releaseConfig from './config'
-export { releaseRules } from './rules'
+import { releaseRules } from './rules'
+
+const plugins = [
+    ['@semantic-release/commit-analyzer', { preset: 'aron', releaseRules }],
+    ['@semantic-release/release-notes-generator', { preset: 'aron' }],
+    ['@semantic-release/exec', {
+        prepareCmd: 'npm run check && npm run build',
+        publishCmd: 'npm version ${nextRelease.version} --workspaces && npm publish --workspaces --access public'
+    }],
+    '@semantic-release/github'
+]
+
+const branches = [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    {
+        name: 'beta',
+        prerelease: true
+    },
+    {
+        name: 'alpha',
+        prerelease: true
+    }
+]
+
+export {
+    plugins,
+    branches,
+    releaseRules
+}
--- release.config.js
+++ release.config.js
@@ -1,17 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line no-undef
-const { releaseRules } = require('semantic-release-config-aron')
-
-// eslint-disable-next-line no-undef
 module.exports = {
-    extends: 'semantic-release-config-aron',
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'aron',
-                releaseRules
-            }
-        ]
-    ]
+    extends: 'semantic-release-config-aron'
 }
