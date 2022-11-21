5087110369914adad39c2d3f8b6ee71fa2fded55
1Aron <aron886tw@gmail.com>
2022-11-22 00:14:52 +0800

    New(Command): Version

--- packages/cli/bin/aron.js
+++ packages/cli/bin/aron.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
+const fs = require('fs-extra')
+const program = require('commander')
+const path = require('path')
+const { version, name, description } = fs.readJSONSync(path.join(__dirname, '../package.json'))
+const fg = require('fast-glob')
 
-console.log('aron')
+program
+    .name(name)
+    .description(description)
+    .version(version | 'dev')
+
+fg.sync(path.join(__dirname, '../commands/*')).forEach((eachCommandPath) => {
+    require(eachCommandPath)
+})
+
+program.parse()
+
---
+++ packages/cli/commands/version.js
@@ -0,0 +1,50 @@
+const program = require('commander')
+const fg = require('fast-glob')
+const fs = require('fs-extra')
+const path = require('path')
+const { workspaces } = fs.readJSONSync('./package.json')
+
+program.command('version <version>')
+    .description('Bump to specific version for workspace\'s packages')
+    .option('-p, --prefix <symbol>', 'version prefix `^`, `~`, `>`, `>=`, `<`, `<=` ', '^')
+    .action((version, { prefix }) => {
+        const workspacePackagesOfPath = {}
+        const workspacePackagesOfName = {}
+        const workspacePackagePaths = workspaces.map((eachWorkspace) => path.join(eachWorkspace, '*package.json'))
+        const updateDependencies = (dependencies) => {
+            let updated = false
+            for (const dependencyName in dependencies) {
+                if (dependencyName in workspacePackagesOfName) {
+                    const dependencyVersion = dependencies[dependencyName]
+                    if (dependencyVersion === '') {
+                        dependencies[dependencyName] = prefix + version
+                        updated = true
+
+                    }
+                }
+            }
+            return updated
+        }
+
+        // Read package.json by workspaces
+        fg.sync(workspacePackagePaths)
+            .forEach((eachWorkspacePackageJSONPath) => {
+                const eachWorkspacePackage = fs.readJSONSync(eachWorkspacePackageJSONPath)
+                workspacePackagesOfPath[eachWorkspacePackageJSONPath] = eachWorkspacePackage
+                workspacePackagesOfName[eachWorkspacePackage.name] = eachWorkspacePackage
+                // Bump to next verion
+                eachWorkspacePackage.version = version
+            })
+
+        for (const eachWorkspacePackageJSONPath in workspacePackagesOfPath) {
+            const eachWorkspacePackage = workspacePackagesOfPath[eachWorkspacePackageJSONPath]
+            const { dependencies, peerDependencies } = workspacePackagesOfPath[eachWorkspacePackageJSONPath]
+            if (
+                dependencies && updateDependencies(dependencies)
+                || (peerDependencies) && updateDependencies(peerDependencies)
+            ) {
+                fs.writeJSONSync(eachWorkspacePackageJSONPath, eachWorkspacePackage)
+            }
+        }
+    })
+
