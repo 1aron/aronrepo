f47d8c771816b064968dea5595e1493fbd512071
1Aron <aron886tw@gmail.com>
2022-11-23 21:16:39 +0800

    Misc: Repo

---
+++ packages/repo/src/bin/index.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import fs from 'fs-extra'
+import program from 'commander'
+import path from 'path'
+import '../commands'
+
+const { version, name, description } = fs.readJSONSync(path.join(__dirname, '../package.json'))
+
+program
+    .name(name)
+    .description(description)
+    .version(version)
+
+program.parse()
+
