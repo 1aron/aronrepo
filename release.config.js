388bdaa78d954eaee769cdf7e18eb8259f17c65f
1Aron <aron886tw@gmail.com>
2022-11-27 01:42:22 +0800

    Misc: Repo

--- release.config.js
+++ release.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-    extends: 'semantic-release-config-aron'
-}
+const configure = require('semantic-release-config-aron/configure')
+
+module.exports = configure({
+    scripts: {
+        prepare: 'npm run check && npm run build',
+        publish: 'npm run version ${nextRelease.version} && npm publish --workspaces --access public'
+    }
+})
