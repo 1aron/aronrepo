008e33feee45b896aad123598df2216b01c1796b
1Aron <aron886tw@gmail.com>
2022-11-28 01:27:05 +0800

    Misc: Test

--- packages/repo/tests/css-args/pack.test.ts
+++ packages/repo/tests/css-args/pack.test.ts
@@ -2,6 +2,6 @@ import { expectExist } from '../../../../utils/expect-exist'
 import { run } from '../../../../utils/run'
 
 test('specify css entries', () => {
-    run('aron pack src/*.css')
+    run('npx aron pack src/*.css')
     expectExist(['dist/index.css', 'dist/float.css', 'dist/heart.css'])
 })
--- packages/repo/tests/css/pack.test.ts
+++ packages/repo/tests/css/pack.test.ts
@@ -3,7 +3,7 @@ import { expectExist } from '../../../../utils/expect-exist'
 import { run } from '../../../../utils/run'
 
 test('extract css entries from `package.json`', () => {
-    run('aron pack')
+    run('npx aron pack')
     expectExist(['dist/index.css'])
     expectFileIncludes('dist/index.css', ['body{background-color:red}'])
 })
