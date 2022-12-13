9da2779ac93d563f0f29f30a87c07e20d549d44d
Miles <swrev5741@gmail.com>
2022-12-13 14:33:54 +0800

    Fix(Github Actions): Compare error when merge

--- packages/github-actions/src/merge.js
+++ packages/github-actions/src/merge.js
@@ -4,25 +4,13 @@ module.exports = async function merge(targetBranch) {
     const client = new github.getOctokit(process.env.GITHUB_TOKEN)
     const headToMerge = process.env.GITHUB_SHA
     const repository = process.env.GITHUB_REPOSITORY
+    const [owner = null, repo = null] = repository.split('/')
 
-    let comparison = client.compare(
-        repository,
-        targetBranch,
-        headToMerge
-    )
-
-    if (comparison.status == 'identical' && process.env.INPUT_DISABLE_FASTFORWARDS == 'true') {
-        console.log(`Skip fastforward merge targetBranch: ${targetBranch} headToMerge: ${headToMerge}`)
-    } else {
-        console.log(`Running perform merge targetBranch: ${targetBranch} headToMerge: ${headToMerge}}`)
-
-        client.merge(
-            repository,
-            targetBranch,
-            headToMerge,
-            process.env.INPUT_MESSAGE ? { commit_message: process.env.INPUT_MESSAGE } : {}
-        )
-
-        console.log(`Finish merge branch ${headToMerge} to ${targetBranch}`)
-    }
+    await client.request('POST /repos/{owner}/{repo}/merges', {
+        owner,
+        repo,
+        base: targetBranch,
+        head: headToMerge,
+        commit_message: `Merge ${headToMerge}`
+    })
 }
