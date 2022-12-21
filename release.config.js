3a93400d5c2e794f0406ebe2f8e0100359b0720c
1Aron <aron@master.co>
2022-12-21 17:55:11 +0800

    Update release

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ const configure = require('semantic-release-config-aron/configure')
 module.exports = configure({
     plugins: {
         '@semantic-release/exec': {
-            prepareCmd: 'npm run check && npm run version ${nextRelease.version}'
+            prepareCmd: 'npm run version ${nextRelease.version}'
         }
     }
 })
